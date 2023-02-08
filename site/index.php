0634cde152ebd2cc770d20502324bb47e6e5f3bb
Mohamed El Harchaui <melharchaui784@gmail>
2023-02-08 14:36:24 +0100

    Opdracht mijn gegevens

--- site/index.php
+++ site/index.php
@@ -1,3 +1,15 @@
 <?php
 
-echo "I am a big fan of the Simpsons!";
+$GetallenLijst = [  
+[12,34,45,65]
+["haas","beer","das"]
+];
+
+echo $GetallenLijst[2];
+
+$mijnGegevens = [
+    "Voornaam:" =>"Mohamed"
+    "Achternaam:" => "El Harchaui"
+    "Leeftijd:" => 22
+]
+?>
