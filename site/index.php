a24266a98888419024fcc55ce4a06e31e4e9da5a
Mohamed El Harchaui <melharchaui784@gmail>
2023-02-08 15:00:57 +0100

    Opdracht 18

--- site/index.php
+++ site/index.php
@@ -1,14 +1,4 @@
 <?php
-$mijnGegevens = ["Mohamed", 78, TRUE, 3.4];
-echo $mijnGegevens[3]
+$abcGetallen = [["M", "O", "R", "E"]][[24, 56, 78, 90]];
 
-$mijnGegevens = [
-    "Voornam" => "Mohamed";
-    "Achternaam" => "El Harchaui";
-    "Leeftijd" => 22;
-];
-
-$mijnGegevens = ["Voornam"] = "Mohamed";
-$mijnGegevens = ["Achternaam"] = "El Harchaui";
-$mijnGegevens = ["Leeftijd"] = 22;
 ?>
