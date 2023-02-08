444c8650edf7e16be16a35029417fe2a5ebb36df
Mohamed El Harchaui <melharchaui784@gmail>
2023-02-08 14:55:56 +0100

    opdracht 17

--- site/index.php
+++ site/index.php
@@ -1,15 +1,14 @@
 <?php
-
-$GetallenLijst = [  
-[12,34,45,65]
-["haas","beer","das"]
-];
-
-echo $GetallenLijst[2];
+$mijnGegevens = ["Mohamed", 78, TRUE, 3.4];
+echo $mijnGegevens[3]
 
 $mijnGegevens = [
-    "Voornaam:" =>"Mohamed"
-    "Achternaam:" => "El Harchaui"
-    "Leeftijd:" => 22
-]
+    "Voornam" => "Mohamed";
+    "Achternaam" => "El Harchaui";
+    "Leeftijd" => 22;
+];
+
+$mijnGegevens = ["Voornam"] = "Mohamed";
+$mijnGegevens = ["Achternaam"] = "El Harchaui";
+$mijnGegevens = ["Leeftijd"] = 22;
 ?>
