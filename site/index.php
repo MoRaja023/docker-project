785310a954ec9300d3a1a4096d1c7eef7a01c041
Mohamed El Harchaui <melharchaui784@gmail>
2023-02-15 14:55:24 +0100

    “opdracht 18”

--- site/index.php
+++ site/index.php
@@ -1,22 +1,11 @@
 <?php
-$spongebob_main_characters = [
-    "Spongebob" => [
-        "age" => 12,
-        "job" => "fry cook"
-    ],
-    "Patrick" => [
-    "age" => 12
-    "job" => "unemployed"
-    ],
-    "Sandy" => [
-    "age" => 25
-    "job" => "scientist"
-    ],
-    "Squidward" => [
-        "age" => 35
-        "job" => "cashier"
-    ]
-    ];
-    echo($spongebob_main_characters["Patrick"]["age"])
-    echo($spongebob_main_characters["Patrick"]["job"])
+$mijngegevens = [
+    "Voornaam" => "Mohamed",
+    
+    "Achternaam" => "El Harchaui",
+
+    "Leeftijd" => 22,
+];
+foreach($mijngegevens as $info)
+echo $info;
 ?>
